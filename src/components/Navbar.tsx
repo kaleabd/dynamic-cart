9e3c3a9ee4daae98b64ef7f4a86d141cd5b930d7
kaleabd <kaleabdereje94@gmail.com>
2023-04-21 13:42:46 +0300

    added counter functionalityto the cart

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,20 @@
+import { useCardStore } from '../store/UseCardStore';
 import './Navbar.css'
 import {Link} from 'react-router-dom';
 
+
 export const Navbar = () => {
+    const {carts} =useCardStore()
     return(
         <div className='nav'>
             <h1>Congo-Shop</h1>
             <ul>
                 <li>Home</li>
-                <Link to="/cart">Cart</Link>
+                <div className='cart-note'>
+                    <Link to="/cart">Cart</Link>
+                    <div className='cart-counter'>{carts.length}</div>
+                </div>
+                
                 <li>Contactus</li>
             </ul>
         </div>
