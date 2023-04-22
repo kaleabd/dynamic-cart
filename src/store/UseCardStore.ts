5784d2c5e69140949652c74c45a2383c17f2990b
kaleabd <kaleabdereje94@gmail.com>
2023-04-22 16:59:19 +0300

    minor fixing on usecardstore

--- src/store/UseCardStore.ts
+++ src/store/UseCardStore.ts
@@ -2,15 +2,11 @@ import {create} from 'zustand'
 import { CardProps } from '../components/CardList'
 
 interface CardState {
-  bears: number
-  increase: (by: number) => void
   carts: CardProps[]
   addCarts: (cart: CardProps) => void
 }
 
 export const useCardStore = create<CardState>((set) => ({
-    bears: 0,
-    increase: (by) => set((state) => ({ bears: state.bears + by })),
     carts: [],
     addCarts: (cart) => set((state) => ({
       carts: [
